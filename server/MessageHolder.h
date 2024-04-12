be82aaccedebc2c3fbd10a12d255f796c50fa403
David Houdek <david.houdek11@gmail.com>
2024-04-12 16:44:55 +0200

    .gitignore: debug

---
+++ server/MessageHolder.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <set>
+#include <condition_variable>
+#include "Message.h"
+
+class MessageHolder {
+public:
+
+    explicit MessageHolder(std::mutex & messagesMutex);
+
+    void addMessage(const Message & message);
+
+    void removeMessage(const Message & message);
+
+    [[nodiscard]] std::set<Message> getMessages() const;
+
+    std::string serializeMessages();
+
+    void clearMessages();
+
+    [[nodiscard]] std::condition_variable & getCallback();
+
+    [[nodiscard]] std::mutex & getMessagesMutex();
+
+
+private:
+    std::set<Message> _messages;
+    std::condition_variable callBackOnMessagesChange;
+    std::mutex & messagesMutex;
+
+    std::string _serializedMessagesCache;
+    bool _changeSinceLastSerialization = false;
+};
