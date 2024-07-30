159af566d6881c3c8906597bd084abf1135189d6
David Houdek <david.houdek11@gmail.com>
2024-07-30 21:57:51 +0200

    debug

--- src/GTKHandler.h
+++ src/GTKHandler.h
@@ -21,7 +21,7 @@ public:
 	struct GtkData {
 
 		void init() {
-			_app = Gtk::Application::create("org.hikari03.blink");
+			_app = Gtk::Application::create("cz.hikari03.blink");
 			_builder = Gtk::Builder::create_from_file("blink.ui");
 			_windowIntro = _builder->get_widget<Gtk::ApplicationWindow>("blink");
 			_windowChat = _builder->get_widget<Gtk::ApplicationWindow>("mainAppWin");
